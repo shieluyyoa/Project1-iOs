673b22edb945338cfcbafcfbb65e6d8a4b69c552
shieluyyoa <luiwinghin8998@gmail.com>
2022-04-14 00:39:11 +0800

    Project1

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var pictures:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        DispatchQueue.global(qos: .background).async {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                self.pictures.append(item)
+            }
+        }
+            self.pictures.sort()
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
+        }
+        
+    
+        // o any additional setup after loading the view.
+    }
+    
+    
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture",for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            vc.selectedPictureNumber = indexPath.row+1
+            vc.totalPictures = pictures.count
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
     }
 
-
-}
 
